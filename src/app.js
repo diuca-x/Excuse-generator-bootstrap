f1d2e5f5628b697b7f86483f7313ddefff0027a7
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-04-14 18:31:37 +0000

    created excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["The dog ", "My granma ", "His turtle ", "My bird "];
+  let action = ["eat ", "pissed ", "crushed ", "broked "];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let string1 = who[Math.floor(Math.random() * who.length)];
+  let string2 = action[Math.floor(Math.random() * action.length)];
+  let string3 = what[Math.floor(Math.random() * what.length)];
+  let string4 = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = string1 + string2 + string3 + string4;
+  document.getElementById("excuse").innerHTML = excuse;
   console.log("Hello Rigo from the console!");
 };
